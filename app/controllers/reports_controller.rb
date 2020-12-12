edd42db1f181e147905c61359c452a7e8b1b98d2
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2020-12-12 11:56:21 -0300

    include job

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,17 +4,8 @@ class ReportsController < ApplicationController
   end
 
   def create
-
-    sleep 8
-    if File.directory?("#{Rails.root.join('public')}/reports").blank?
-      Dir.mkdir "#{Rails.root.join('public')}/reports"
-    end
-    code = (0...8).map { (65 + rand(26)).chr }.join
-    out_file = File.new("#{Rails.root.join('public')}/reports/report#{code}.html", "w")
-    out_file.puts("<p>Your report here - code: <b>#{code}</b></p>")
-    out_file.puts("<hr>")
-    out_file.close
-
+    ReporteJob.perform_later
+    flash[:notice] = "Precessing..."
     redirect_to '/reports'
   end
 end
